5e03e38fc31a78c2cdc2709a3e7f89fd7eadaec9
Nate Fastar <fastar3@gmail.com>
2020-02-22 16:49:28 -0700

    New Functionality
    Added error catching for misspelled words, a new list is displayed containing possible words they might have meant

--- script.js
+++ script.js
@@ -10,31 +10,31 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
       return response.json();
     }).then(function(json) {
       let results = "";
-	  console.log(json);
-      results += '<h2>' + json.length + " definitions found for " + value + "</h2>"
-	  document.getElementById("weatherResults").innerHTML = results;
 	  let forcast = "";
-	  forcast += "<div class='dayColumn'>";
-	  for (let i=0; i < json.length; ++i)
-	  {
-		  forcast += "<div class='card mb-3'>";
-		  forcast += "<div class='row no-gutters' style='width:95%;'>";
-		  forcast += "<div class='col-md-4'>";
-		  forcast += "<h2 class='card-title'>" + (json[i].hwi.hw).replace(/^\w/, c => c.toUpperCase()) + ", " + (json[i].fl).replace(/^\w/, c => c.toUpperCase()) + "</h2>";
-		  forcast += "</div>";
-		  forcast += "<div class='col-md-8'>";
-		  forcast += "<ul>";
-		  for (let j=0; j < json[i].shortdef.length; ++j)
-		  {
-			  forcast += "<li>" + (json[i].shortdef[j]).replace(/^\w/, c => c.toUpperCase()) + ".</li>";
-		  }
-		  forcast += "</ul>";
-		  forcast += "</div>";
-		  forcast += "</div>";
-		  forcast += "</div>";
-	  }
-	  forcast += "</div>";
-	  
+	  console.log(json);
+	  if(json[0].meta != null){
+		results += '<h2>' + json.length + " definitions found for " + value + "</h2>";	
+		forcast += "<div class='dayColumn'>";
+		for (let i=0; i < json.length; ++i)
+		{
+			forcast += "<div class='card mb-3'>";
+			forcast += "<div class='row no-gutters' style='width:95%;'>";
+			forcast += "<div class='col-md-4'>";
+			forcast += "<h2 class='card-title'>" + (json[i].hwi.hw).replace(/^\w/, c => c.toUpperCase()) 	+ ", " + (json[i].fl).replace(/^\w/, c => c.toUpperCase()) + "</h2>";
+			forcast += "</div>";
+			forcast += "<div class='col-md-8'>";
+			forcast += "<ul>";
+			for (let j=0; j < json[i].shortdef.length; ++j)
+			{
+				forcast += "<li>" + (json[i].shortdef[j]).replace(/^\w/, c => c.toUpperCase()) + ".</li>";
+			}
+			forcast += "</ul>";
+			forcast += "</div>";
+			forcast += "</div>";
+			forcast += "</div>";
+		}
+		forcast += "</div>";
+		
  //     for (let i=0; i < json.weather.length; i++) {
 //	results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
   //    }
@@ -48,6 +48,28 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
 	  //results += "<br/>"+ json.main.humidity + "% Humidity";
 	  //results += "<br/>Low: "+ json.main.temp_min + "&deg;F High: "+ json.main.temp_max + "&deg;F";
      // results += "</p>";
-      document.getElementById("forecastResults").innerHTML = forcast;
-    });
+     
+    }
+	else{
+		results += "<h2>No definitions found for " + value + "</h2>";
+		if(json.length > 0)
+		{
+			forcast += "<div class='card' style='width:50%;'>";
+			forcast += "<h2>Similar words to " + value + " include:</h2>";
+			forcast += "<ul>";
+			for(let i = 0; i < json.length; ++i)
+			{
+				forcast += "<li style='font-size:20px;'>" +json[i]+ "</li>"
+			}
+			forcast += "</ul>";
+			forcast += "</div>";
+		}
+		else
+		{
+			forcast += "<h2>No similar words found.</h2>";
+		}
+	}
+	 document.getElementById("forecastResults").innerHTML = forcast;
+	document.getElementById("weatherResults").innerHTML = results;
+	});
 });
