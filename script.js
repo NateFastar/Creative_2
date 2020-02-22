3cb8b01bfe858189f3ea302dbb731c299f2eb97d
Nate Fastar <fastar3@gmail.com>
2020-02-21 17:43:40 -0700

    Dropped in the project
    Had to scrap it a few times, but the final product works.

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const value = (document.getElementById("weatherInput").value).replace(/^\w/, c => c.toUpperCase());
+  if (value === "")
+    return;
+  console.log(value);
+    const url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + value + "?key=903aa366-0a28-4438-8064-c395172f2817";
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      let results = "";
+	  console.log(json);
+      results += '<h2>' + json.length + " definitions found for " + value + "</h2>"
+	  document.getElementById("weatherResults").innerHTML = results;
+	  let forcast = "";
+	  forcast += "<div class='dayColumn'>";
+	  for (let i=0; i < json.length; ++i)
+	  {
+		  forcast += "<div class='card mb-3'>";
+		  forcast += "<div class='row no-gutters' style='width:95%;'>";
+		  forcast += "<div class='col-md-4'>";
+		  forcast += "<h2 class='card-title'>" + (json[i].hwi.hw).replace(/^\w/, c => c.toUpperCase()) + ", " + (json[i].fl).replace(/^\w/, c => c.toUpperCase()) + "</h2>";
+		  forcast += "</div>";
+		  forcast += "<div class='col-md-8'>";
+		  forcast += "<ul>";
+		  for (let j=0; j < json[i].shortdef.length; ++j)
+		  {
+			  forcast += "<li>" + (json[i].shortdef[j]).replace(/^\w/, c => c.toUpperCase()) + ".</li>";
+		  }
+		  forcast += "</ul>";
+		  forcast += "</div>";
+		  forcast += "</div>";
+		  forcast += "</div>";
+	  }
+	  forcast += "</div>";
+	  
+ //     for (let i=0; i < json.weather.length; i++) {
+//	results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+  //    }
+    //  results += '<h2>' + json.main.temp + " &deg;F, Feels Like: " + json.main.feels_like + " &deg;F</h2>"
+    //  results += "<p>"
+//      for (let i=0; i < json.weather.length; i++) {
+	//results += json.weather[i].description
+	//if (i !== json.weather.length - 1)
+	//  results += ", "
+     // }
+	  //results += "<br/>"+ json.main.humidity + "% Humidity";
+	  //results += "<br/>Low: "+ json.main.temp_min + "&deg;F High: "+ json.main.temp_max + "&deg;F";
+     // results += "</p>";
+      document.getElementById("forecastResults").innerHTML = forcast;
+    });
+});
